32ac996774269ad994f704ab8be92c8bc01e938b
hyjoo1226 <sparrow9576@gmail.com>
2026-04-02 14:57:42 +0900

    Feat: 퀴즈 클래스 작성

---
+++ models.py
@@ -0,0 +1,60 @@
+# 클래스 정의
+
+class Quiz:
+    # Quiz 객체 초기화
+        # quiz_id: 문제 고유 ID (int)
+        # question: 문제 내용 (str)
+        # choices: 4개의 선택지 (list of str)
+        # answer: 정답 번호 1~4 (int)
+        # hint: 문제 관련 힌트 (str, optional)        
+    def __init__(self, quiz_id, question, choices, answer, hint=None):
+        self.id = quiz_id
+        self.question = question
+        self.choices = choices
+        self.answer = answer
+        self.hint = hint
+
+    # 문제 출력 메서드
+        # index: 문제 번호 (int)
+    def display_quiz(self, index):
+        print(f"\n[문제 {index}] {self.question}")
+        for i, choice in enumerate(self.choices, 1):
+            print(f"{i}. {choice}")
+
+    # 힌트 출력 메서드
+    def display_hint(self):
+        if self.hint:
+            print(f"💡 힌트: {self.hint}")
+        else:
+            print("이 문제에는 힌트가 없습니다.")
+
+    # 정답 확인 메서드
+        # user_input: 사용자가 입력한 번호 (int)
+    def is_correct(self, user_input):
+        return user_input == self.answer
+
+    # 데이터 불러오기(JSON 데이터에서 Quiz 객체 생성)
+        # data: JSON 데이터 (dict)
+    @classmethod
+    def from_dict(cls, data):
+        quiz_id=data.get("id")
+        if quiz_id is None:
+            raise ValueError("퀴즈 ID가 없습니다.")
+        
+        return cls(
+            quiz_id=quiz_id,
+            question=data["question"],
+            choices=data["choices"],
+            answer=data["answer"],
+            hint=data.get("hint"),
+        )
+
+    #  데이터 저장하기 메서드(Quiz 객체를 JSON 데이터로 변환)
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "question": self.question,
+            "choices": self.choices,
+            "answer": self.answer,
+            "hint": self.hint
+        }
